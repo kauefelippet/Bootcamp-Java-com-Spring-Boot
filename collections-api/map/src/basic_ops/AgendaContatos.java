d826ac6f8f66afb381dd386e225ba2513e039cba
kauefelippet <kaue.andrea.elias@gmail.com>
2024-08-07 21:36:52 -0300

    add Collections API Map exercises
    Signed-off-by: kauefelippet <kaue.andrea.elias@gmail.com>

---
+++ collections-api/map/src/basic_ops/AgendaContatos.java
@@ -0,0 +1,52 @@
+package basic_ops;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if (!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContatos() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Kaue", 123456);
+        agendaContatos.adicionarContato("Kaue", 5665);
+        agendaContatos.adicionarContato("Kaue Felippe", 11111111);
+        agendaContatos.adicionarContato("Kaue Tiburcio", 654987);
+        agendaContatos.adicionarContato("Maria Silva", 11111111);
+        agendaContatos.adicionarContato("Kaue", 44444);
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.removerContato("Maria Silva");
+        agendaContatos.exibirContatos();
+
+    }
+}
